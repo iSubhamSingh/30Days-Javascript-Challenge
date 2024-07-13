40c43bef4b111d0533148c36e4e93d0069f1f029
Subham <subhamsingh5638@gmail.com>
2024-07-13 14:05:59 +0530

    first day completed

---
+++ Day1_Variables_DataTypes.js
@@ -0,0 +1,42 @@
+//Activity 1
+var first = 10;
+console.log(first);
+
+let str = "Hello"
+console.log(str);
+
+//Activity 2
+const bool = true;
+console.log(bool);
+
+//Activity 3
+let num = 10;
+let stringVar = "String value";
+let boolVar = false;
+let obj = {
+    id: 10,
+    email: "user@email.com"
+};
+let arr = [1,4,9,"lorem"];
+
+console.log(typeof(num));
+console.log(typeof(stringVar));
+console.log(typeof(boolVar));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+//Activity 4
+let initialValue = 15;
+console.log(`Initial value : ${initialValue}`);
+newValue = 20
+console.log(`New value: ${newValue}`);
+
+//Activity 5
+const constantValue = "Constant";
+console.log(constantValue);
+try {
+    constantValue = "change";
+} catch (error) {
+    console.log(error);
+}
+
