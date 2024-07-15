34c682de31afe8396074c080e9e7fcdc07594524
Subham <subhamsingh5638@gmail.com>
2024-07-15 23:35:53 +0530

    Completed day 2

---
+++ Day2_Operators.js
@@ -0,0 +1,36 @@
+//Activity 1
+
+let first = 50;
+let second = 20;
+
+console.log(first + second);
+console.log(first - second);
+console.log(first * second);
+console.log(first / second);
+console.log(first % second);
+
+//Activity 2
+
+first += 10;
+console.log(first);
+second -= 10;
+console.log(second);
+
+//Activity 3
+
+console.log(first > 10 && second < 30);
+console.log(first >= 10 && second <=20);
+console.log(first == second);
+console.log(first === second);
+
+//Activity 4
+
+console.log(first >= 10 || second <= 20);
+console.log(!first == 10);
+
+//Activity 5
+let pos = 5;
+let neg = -15;
+
+(pos >= 0) ? console.log("Number is positive") : console.log("Number is negative");;
+
