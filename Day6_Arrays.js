85ec087d20854590976217de9d6372608b2bdd29
Subham <subhamsingh5638@gmail.com>
2024-07-25 21:10:27 +0530

    completed day 6 assignment

--- Day6_Arrays.js
+++ Day6_Arrays.js
@@ -29,3 +29,38 @@ console.log(square);
 //Task8
 let even = numbers.filter(num => num % 2 === 0)
 console.log(even);
+
+//Task9
+let sum = numbers.reduce((accumulator, number) => {
+  return accumulator += number;
+},0)
+console.log(`Sum is ${sum}`);
+
+
+//Activity4
+//Task10
+console.log("Elements of array:");
+for(let i = 0; i < numbers.length; i++){
+  console.log(numbers[i]);
+}
+
+//Task11
+console.log("Elements of the array iterated using for each loop: ");
+numbers.forEach(number => {
+  console.log(number);
+})
+
+
+
+//Acitvity5
+//Task12
+console.log("Element of multidimenstional array : ");
+let mdArray =[[1,2,3],[4,5,6],[7,8,9]];
+for(let i = 0; i < mdArray.length; i++){
+  for(let j = 0; j < mdArray[i].length; j++){
+    console.log(mdArray[i][j]);
+  }
+}
+
+//Task13
+console.log(mdArray[0][1]);
