9ee1934235913315c48f8475a2b4895b0c4b5454
Subham <subhamsingh5638@gmail.com>
2024-07-27 19:55:20 +0530

    Completed Day 7

--- Day7_Objects.js
+++ Day7_Objects.js
@@ -3,7 +3,10 @@
 let books = {
     title : "The Alchemist",
     author : "Paulo Coelho",
-    year : 1988
+    year : 1988,
+    titleYear : function(){
+        return `Title : ${this.title},  Year : ${this.year}`
+    }
 }
 console.log(books);
 
@@ -26,3 +29,51 @@ function updateYear(year){
 updateYear(1989)
 console.log(books);
 
+//Activity 3
+//Task 5
+let library = {
+    name : "Computer Science",
+    books: [
+        {
+            title: "The Self-Taught Computer Scientist ",
+            author: "Cory Althoff",
+            year: 2021
+        },
+        {
+            title: "A Programmer’s Guide to Computer Science",
+            author: "Dr. William Springer",
+            year: 2019
+        },
+        {
+            title: "The Pragmatic Programmer ",
+            author: "Andrew Hunt",
+            year: 1999
+        }
+    ]
+}
+
+console.log(library);
+
+//Task 6
+console.log(`Name of the library : ${library.name}`);
+console.log("Titles of all the books in the library : ");
+library.books.forEach(obj => {
+    console.log(obj.title);
+})
+
+
+//Activity 4
+//Task 7
+console.log(`Title and year of the book : ${books.titleYear()}`);
+
+
+//Activity 5
+//Task 8
+console.log("Printing properties of the book");
+for (const key in books) {
+    console.log(`${key} : ${books[key]}`);
+}
+
+//Task 9
+console.log("All the keys and values of the object : ");
+console.log(`Keys: ${Object.keys(books)},  Values: ${Object.values(books)}`);
