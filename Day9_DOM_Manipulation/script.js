5e827cfc6b11e35f4cf0f393bc9428fb677f50c4
Subham <subhamsingh5638@gmail.com>
2024-07-31 21:40:02 +0530

    day9

--- Day9_DOM_Manipulation/script.js
+++ Day9_DOM_Manipulation/script.js
@@ -1,1 +1,6 @@
-document.getElementById('header').textContent = "Changed Heading"
+//Activity 1
+//Task 1
+document.getElementById('header').textContent = "Changed Heading";
+
+//Task 2
+document.querySelector('.para').style.backgroundColor = 'red';
