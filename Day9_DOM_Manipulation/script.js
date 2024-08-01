f116292514ed94cbbc3cc15c83a27aaad1982659
Subham <subhamsingh5638@gmail.com>
2024-08-01 23:07:37 +0530

    day 9

--- Day9_DOM_Manipulation/script.js
+++ Day9_DOM_Manipulation/script.js
@@ -4,3 +4,9 @@ document.getElementById('header').textContent = "Changed Heading";
 
 //Task 2
 document.querySelector('.para').style.backgroundColor = 'red';
+
+//Activity 2
+//Task 3
+let div = document.createElement('div');
+div.textContent = 'Text inside div';
+document.body.appendChild(div);
