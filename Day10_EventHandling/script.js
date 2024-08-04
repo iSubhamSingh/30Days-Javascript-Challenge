f442c072ef8ddad69c01489699b924b263df6fdf
Subham <subhamsingh5638@gmail.com>
2024-08-04 22:30:26 +0530

    day10

---
+++ Day10_EventHandling/script.js
@@ -0,0 +1,8 @@
+//Activity 1
+//Task 1
+document.getElementById('btn').addEventListener('click', () => {
+    document.getElementById('para').textContent = "Text content of paragraph changed on click of the button";
+})
+
+//Task 2
+document.querySelector('img').
