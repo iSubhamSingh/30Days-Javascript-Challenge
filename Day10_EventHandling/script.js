b08dadf5f7192760b0c9f08b2572957a57900f89
Subham <subhamsingh5638@gmail.com>
2024-08-05 19:58:09 +0530

    day10

--- Day10_EventHandling/script.js
+++ Day10_EventHandling/script.js
@@ -5,4 +5,10 @@ document.getElementById('btn').addEventListener('click', () => {
 })
 
 //Task 2
-document.querySelector('img').
+//toggle image visibility
+let img = document.getElementById('image');
+img.addEventListener('dblclick', () => {
+    img.style.visibility = (img.style.visibility == 'hidden') ? 'visible' : 'hidden';   
+
+})
+
