aee0c5c9bdcd60b5f5cd863bcd61b5925007d88b
Subham <subhamsingh5638@gmail.com>
2024-08-07 23:16:19 +0530

    Completed Day 10

--- Day10_EventHandling/script.js
+++ Day10_EventHandling/script.js
@@ -12,10 +12,65 @@ img.addEventListener('dblclick', () => {
 
 })
 
+
+//Activity 2
 //Task 3
-document.getElementById('para').addEventListener('mouseover', () => {
-    document.getElementById('para').style.color = 'red';
+document.getElementById('para').addEventListener('mouseenter', () => {
+    document.getElementById('para').style.backgroundColor = 'red';
 })
 
 //Task 4
-document.getElementById('para').addEventListener()
+document.getElementById('para').addEventListener('mouseout', () => {
+    document.getElementById('para').style.backgroundColor = 'white';
+})
+
+
+//Activity 3
+//Task 5
+let inputField = document.getElementById('input');
+inputField.addEventListener('keydown', (event) => {
+    console.log(event.key);
+})
+
+//Task 6
+inputField.addEventListener('keyup', () => {
+    document.getElementById('paragraph').textContent = inputField.value ;
+})
+
+
+//Activity 4
+//Task 7
+let form = document.getElementById('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(`Name : ${document.getElementById('name').value}`);
+    console.log(`Gender : ${document.getElementById('dropdown').value}`);
+})
+
+
+//Task 8
+document.getElementById('dropdown').addEventListener('change', () => {
+    document.getElementById('selectPara').textContent = document.getElementById('dropdown').value;
+})
+
+
+
+
+//Activiy 5
+//Task 9
+let list = document.getElementById('list');
+list.addEventListener('click', (event) => {
+    if(event.target.tagName === 'LI'){
+        console.log(event.target.textContent);
+        
+    }
+})
+
+
+//Task 10
+let Addbtn = document.getElementById('Addbtn');
+
+Addbtn.addEventListener('click', () => {
+    let li = document.createElement('li');
+    li.textContent = `Item ${list.children.length + 1}`;
+    list.appendChild(li);
+})
