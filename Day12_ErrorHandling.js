66771414239573fef0d89cd2c1ab5b250293f97b
Subham <subhamsingh5638@gmail.com>
2024-08-24 22:21:34 +0530

    day12

--- Day12_ErrorHandling.js
+++ Day12_ErrorHandling.js
@@ -10,3 +10,19 @@ try {
     console.log("Error handled", error.message);
 }
 
+
+//Task 2
+
+function divideNumbers(a,b) {
+    if (b == 0) {
+        throw new Error("Can't divide by 0");
+    }
+    return a/b;
+}
+
+try {
+    console.log(divideNumbers(10, 2));
+    console.log(divideNumbers(10, 0));
+}catch (error) {
+    console.log("Error", error.message);
+}
